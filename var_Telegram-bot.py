d010ce52a114ecebcce5a8947d853851e3e9d0ab
Federico Garcia del Rio <fedex6@gmail.com>
2019-07-12 21:56:58 -0300

    Primeros Pasos
    primer upload

---
+++ var_Telegram-bot.py
@@ -0,0 +1,42 @@
+#
+#	Telegram: @fedex6
+#
+#---------------------#
+
+## Basic imports & configuration
+import os
+import sys
+import time
+import requests
+import json
+import datetime
+import telepot
+
+## Bot data
+token       =   '-- TOKEN --'
+
+def handle(msg):
+    chat_id = msg['chat']['id']
+    command = msg['text']
+
+if command.startwith('/var'):
+    admins = getChatAdministrators(chat_id)
+    for a in admins:
+        call_var_msg += admins
+
+    bot.sendMessage(chat_id, 'Estan pidiendo VAR '+ call_var_msg)
+
+bot = telepot.Bot(token) ## Poner el Token del bot en configuracion.py
+bot.message_loop(handle)
+
+while 1:
+    try:
+        time.sleep(0.5)
+
+    except KeyboardInterrupt:
+        ##LOG - Deja registro de que se freno el programa
+        log = open("log.txt", "a")
+        log.write('[ ' + time.ctime() + ' ] >>> Stoping...\n')
+        log.close()
+        print('\n Program interrupted')
+        exit()
